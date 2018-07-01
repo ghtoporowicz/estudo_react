10d174d955af91498661620c93940c1eefddbbd0
Gustavo Toporowicz <gh.toporowicz@gmail.com>
2018-07-01 13:47:12 -0300

    Conclusão das funcionalidades

--- todo-app/frontend/src/template/menu.jsx
+++ todo-app/frontend/src/template/menu.jsx
@@ -1,19 +1,21 @@
-    import React from 'react'
+import React from 'react'
 
-    export default props => (
-     <nav className='navbar navbar-inverse bg-inverse'>
-        <div className='container'>
-            <div className='navbar-header'>
-                <a className='navbar-brand' href='#'>
-                    <i className='fa fa-calendar-check-o'></i>
-                </a>
+export default props => (
+    <div className='row'>
+        <nav className='navbar navbar-inverse bg-inverse'>
+            <div className='container'>
+                <div className='navbar-header'>
+                    <a className='navbar-brand' href='#'>
+                        <i className='fa fa-calendar-check-o'></i>
+                    </a>
+                </div>
+                <div id='navbar' className='navbar-collapse collapse'>
+                    <ul className='nav navbar-nav'>
+                        <li><a href='#/todos'>Tarefas</a></li>
+                        <li><a href='#/about'>Sobre</a></li>
+                    </ul>
+                </div>
             </div>
-            <div id='navbar' className='navbar-collapse collapse'>
-                <ul className='nav navbar-nav'>
-                    <li><a href='#/todos'>Tarefas</a></li>
-                    <li><a href='#/about'>Sobre</a></li>
-                </ul>
-            </div>
-        </div>
-     </nav>   
-    )
+        </nav>
+    </div>
+)
--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -13,15 +13,25 @@ export default class Todo extends Component {
         this.state = { description: '', list: [] }
 
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleSearch = this.handleSearch.bind(this)
         this.handleChange = this.handleChange.bind(this)
+        this.handleClear = this.handleClear.bind(this)
+
         this.handleRemove = this.handleRemove.bind(this)
-        
+        this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
+        this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
+
         this.refresh()
     }
 
-    refresh() {
-        axios.get(`${URL}?sort=-createdAt`)
-            .then((resp) => this.setState({...this.state, description: '', list: resp.data}))
+    refresh(description = '') {
+        const search = description ? `&description__regex=/${description}/` : ''
+        axios.get(`${URL}?sort=-createdAt${search}`)
+            .then((resp) => this.setState({...this.state, description, list: resp.data}))
+    }
+
+    handleSearch() {
+        this.refresh(this.state.description)
     }
 
     handleChange(e) {
@@ -36,7 +46,21 @@ export default class Todo extends Component {
 
     handleRemove(todo) {
         axios.delete(`${URL}/${todo._id}`)
-            .then(resp => this.refresh())
+            .then(resp => this.refresh(this.state.description))
+    }
+
+    handleMarkAsDone(todo) {
+        axios.put(`${URL}/${todo._id}`, {...todo, done: true })
+            .then(resp => this.refresh(this.state.description))
+    }
+
+    handleMarkAsPending(todo) {
+        axios.put(`${URL}/${todo._id}`, {...todo, done: false })
+            .then(resp => this.refresh(this.state.description))
+    }
+
+    handleClear() {
+        this.refresh()
     }
 
     render() {
@@ -46,9 +70,14 @@ export default class Todo extends Component {
                 <TodoForm 
                     description={this.state.description}
                     handleChange={this.handleChange}
-                    handleAdd={this.handleAdd} />
-                <TodoList list={this.state.list} 
-                          handleRemove={this.handleRemove} />    
+                    handleAdd={this.handleAdd} 
+                    handleSearch={this.handleSearch}
+                    handleClear={this.handleClear   } />
+                <TodoList 
+                    list={this.state.list} 
+                    handleRemove={this.handleRemove} 
+                    handleMarkAsDone={this.handleMarkAsDone}
+                    handleMarkAsPending={this.handleMarkAsPending}/>    
             </div>
         )
     }
